31509ce8fd0cac0d681295ea731017b7c4a4c5dc
April Peng <helloaprilpeng@gmail.com>
2016-06-27 17:57:56 -0700

    create count array that transforms divisible numbers by 3, 5, and 15 to ping, pong, and pingpong respectively. try to finish show array as ul

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,32 @@
 var pingpong = function(x) {
-  return false;
+  var count = [];
+  var countList = document.getElementById('countList');
+  var countElement;
+
+  for (var i = 1; i <= x; i++) {
+    count.push(i);
+    if (i % 3 === 0) {
+      var ping = count.splice (i,1,"ping");
+    }
+    if (i % 5 === 0) {
+      var pong = count.splice (i,1,"pong");
+    }
+    if (i % 15 === 0) {
+      var pingpong = count.splice (i,1,"pingpong");
+    }
+
+    var frag = document.createDocumentFragment();
+    for (var j = count.length; j--;) {
+      var li = document.createElement("li").firstChild;
+      li.appendChild(document.createTextNode(count[j++]));
+      frag.appendChild(countElement);
+    }
+
+
+  }
+
+  alert(count);
+  countList.appendChild(frag);
 
 }
 
@@ -7,10 +34,10 @@ var pingpong = function(x) {
 $(function() {
   $("form#pingpong").submit(function(event) {
     event.preventDefault();
-    
+
     var number = parseInt($("input#number").val());
 
     var result = pingpong(number);
-    alert(result);
+
   });
 });
