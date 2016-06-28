38c59ba0bf4daee2ac12d90f2b0799a7a0dee82f
April Peng <helloaprilpeng@gmail.com>
2016-06-27 23:29:04 -0700

    flesh out js to print ul in index. add ul class section in index.html

--- js/scripts.js
+++ js/scripts.js
@@ -6,27 +6,29 @@ var pingpong = function(x) {
   for (var i = 1; i <= x; i++) {
     count.push(i);
     if (i % 3 === 0) {
-      var ping = count.splice (i,1,"ping");
+      //var ping = count.splice (i,1,"ping"); -->(another method)
+      count[i] = "ping";
     }
     if (i % 5 === 0) {
-      var pong = count.splice (i,1,"pong");
+      count[i] = "pong";
     }
     if (i % 15 === 0) {
-      var pingpong = count.splice (i,1,"pingpong");
+      count[i] = "pingpong";
     }
 
-    var frag = document.createDocumentFragment();
+    $("ul#countList").append("<li><span class='count'>" + count[i] + "</span></li>")
+    /*var frag = document.createDocumentFragment();
     for (var j = count.length; j--;) {
       var li = document.createElement("li").firstChild;
       li.appendChild(document.createTextNode(count[j++]));
       frag.appendChild(countElement);
-    }
+    } */
 
 
   }
 
   alert(count);
-  countList.appendChild(frag);
+//  countList.appendChild(frag);
 
 }
 
